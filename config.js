141d063b3f290474bbf108492720f1f98f64d43b
yblucifer <155779897+yblucifer@users.noreply.github.com>
2024-04-01 16:54:20 +0100

    config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 
 
 
-global.devs = "2349120730021" // Developer Contact
+global.devs = "234906941932." // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "2349069419329";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2349069419329";
 
